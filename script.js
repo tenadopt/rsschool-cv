8e07a2720c4c82c09cad7e25e4bbbf8253334d5b
Tenadopt <yury.ignatiev@gmail.com>
2024-11-16 22:25:23 +0100

    fix: fix minor styles issues

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const toggleMenu = () => {
         navbar.classList.toggle("open");
+        menuToggle.classList.toggle("open");
         mask.classList.toggle("open");
     }
 
     const closeMenu = () => {
         navbar.classList.remove("open");
+        menuToggle.classList.remove("open");
         mask.classList.remove("open");
     }
 
     menuToggle.addEventListener("click", toggleMenu);
     mask.addEventListener("click", toggleMenu);
+    navbar.addEventListener("click", closeMenu);
 
     navLinks.forEach(link => {
         link.addEventListener("click", closeMenu);
